daa448ca3894a1adad4cb4cbb88a6b760ae68edd
Alexandre Zamarion Cepeda <alexandre@MacBook-Air-de-Alexandre.local>
2021-06-30 00:04:06 -0300

    variadic function

--- variadic.c
+++ variadic.c
@@ -6,7 +6,7 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 18:54:24 by azamario          #+#    #+#             */
-/*   Updated: 2021/06/29 23:51:33 by azamario         ###   ########.fr       */
+/*   Updated: 2021/06/30 00:03:41 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,22 @@ variaric funcion reduces code, it's a single function that processes
 variable number of arguments
 
 example:
+int add(int args, ...);
+first param = fixed number of arguments, second param = the variable number of arguments
+
 add(3, 1, 2, 3) = 6
 add(6, 20, 20, 20, 10, 20, 10) = 100
 
+Important steps:
 1 - include stdarg.h to get necessary things to implement variadic functons
 2 - define function prototipe:
     first argument: fixed parameter
-    second argument: elipses (...) -> indicates to the compilet that function 
-    accepts variable number of arguments
+    second argument: elipses (...) -> indicates to the compiler that function accepts variable number of arguments
 3 - define a va_list variable
-4 - begin and end argument list with va_start and va_end (va = variable)
-    macros that recognize where argument list starts and ends
+4 - begin and end argument list with va_start and va_end (va = variable): macros that recognize where argument list starts and ends
 
-
-a - first param = fixed number os arguments, second param = the variable number of arguments)
+Code below:
+a - first param = fixed number of arguments, second param = the variable number of arguments
 b - type va_list is used for argument pointer variables, it points to first optional argument
 c - va_start: this macro initializes the argument pointer variable ap to point to the first of the optional arguments of the current function
     You access the optional arguments by successive calls to va_arg. The first call to va_arg gives you the first optional argument, 
@@ -38,6 +40,9 @@ d - The va_arg macro returns the value of the next optional argument, and modifi
 e - This ends the use of ap. After a va_end call, further va_arg calls with the same ap may not work. You should invoke va_end 
     before returning from the function in which va_start was invoked with the same ap argument.
 f - after final of argument list, we have our return
+
+libc manual: variadic functions
+https://www.gnu.org/software/libc/manual/html_node/Variadic-Functions.html
 */
 
 
@@ -63,7 +68,13 @@ int add(int args, ...)              //a
 int main(void)
 {
     printf("Add1 = %i\n", add(3, 1, 2, 3));
-    printf("Add1 = %i\n", add(6, 20, 20, 20, 10, 10, 20));
+    printf("Add2 = %i\n", add(6, 20, 20, 20, 10, 10, 20));
 
     return (0);
 }
+
+/*
+Output:
+Add1 = 6
+Add2 = 100
+*/
