8a6c9946c12c2feaf9939e9ebca25ef733b493f5
Alexandre Zamarion Cepeda <alexandre@MacBook-Air-de-Alexandre.local>
2021-07-15 17:47:20 -0300

    norminette OK

--- ft_printf.c
+++ ft_printf.c
@@ -6,64 +6,66 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 19:24:00 by azamario          #+#    #+#             */
-/*   Updated: 2021/07/14 09:24:36 by azamario         ###   ########.fr       */
+/*   Updated: 2021/07/15 17:40:53 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void handle_types(int *len, va_list args, t_flags fl)
+static void	handle_types(int *len, va_list args, t_flags fl)
 {
-    if (fl.type == 'c')
-        print_c(va_arg(args, int), len);
-    if (fl.type == 's')
-        print_s(va_arg(args, char *), len);
-    if (fl.type == 'i' || fl.type == 'd')
-        print_i_d(fl, args, len);
-    if (fl.type == 'u')
-        print_u(fl, args, len);
-    if (fl.type == 'p')
-        print_p(fl, args, len);
-    if (fl.type == 'x')
-        print_x(fl, args, len);
-    if (fl.type == 'X')
-        print_X(fl, args, len);
-    if (fl.type == '%')
-        print_pct(len);
+	if (fl.type == 'c')
+		print_c(va_arg(args, int), len);
+	if (fl.type == 's')
+		print_s(va_arg(args, char *), len);
+	if (fl.type == 'i' || fl.type == 'd')
+		print_i_d(fl, args, len);
+	if (fl.type == 'u')
+		print_u(fl, args, len);
+	if (fl.type == 'p')
+		print_p(fl, args, len);
+	if (fl.type == 'x')
+		print_x(fl, args, len);
+	if (fl.type == 'X')
+		print_X(fl, args, len);
+	if (fl.type == '%')
+		print_pct(len);
 }
 
-static void get_specs(const char *format, int  *i, int *len, va_list args) {
-    t_flags fl;
-    if (ft_strchr_01(CONVERSIONS, format[*i]))
-    {
-         fl.type = format[(*i)++];
-         handle_types(len, args, fl);
-    }    
+static void	get_specs(const char *format, int *i, int *len, va_list args)
+{
+	t_flags	fl;
+
+	if (ft_strchr_01(CONVERSIONS, format[*i]))
+	{
+		fl.type = format[(*i)++];
+		handle_types(len, args, fl);
+	}
 }
 
-int ft_printf(const char *format, ...)
+int	ft_printf(const char *format, ...)
 {
-    va_list args;
-    int len;
-    int i;
+	va_list	args;
+	int		len;
+	int		i;
 
-    va_start(args, format);
-    len = 0;
-    i = 0;
-    while (format[i] != '\0')
-    {
-        if (format[i] != '%')
-            ft_putchar_len(format[i++], &len); // tamanho = hello %-s, name // 7
-        else
-        {
-            i++;
-            get_specs(format, &i, &len, args);
-            if (len == -1)
-				return (-1); // %[flags][width][.precision][size]type -> "hello" "    hello"
-        }
-    }
-    va_end(args);
-    return(len);
+	va_start(args, format);
+	len = 0;
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+			ft_putchar_len(format[i++], &len);
+		else
+		{
+			i++;
+			get_specs(format, &i, &len, args);
+			if (len == -1)
+				return (-1);
+		}
+	}
+	va_end(args);
+	return (len);
 }
 
 int main (void)
@@ -74,6 +76,7 @@ int main (void)
     int dec = 42;
     unsigned int ui = -300;
     int hexa = 30000000;
+
 
     printf("\n   printf -> char: %c, string: %s, int: %i, decimal: %d, unsigned: %u\n", c, s, in, dec, ui);
     ft_printf("ft_printf -> char: %c, string: %s, int: %i, decimal: %d, unsigned: %u\n\n", c, s, in, dec, ui);
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 19:24:14 by azamario          #+#    #+#             */
-/*   Updated: 2021/07/14 09:24:17 by azamario         ###   ########.fr       */
+/*   Updated: 2021/07/15 17:46:42 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,21 @@
 # include <stdio.h>
 # include "stdlib.h"
 # include "string.h"
-
-
 # define CONVERSIONS	"cspdiuxX%"
 # define ALL_FLAGS "-0.0123456789cspdiuxX%"
 # define NUMBERS		"0123456789"
 
-typedef struct 
+typedef struct t_flags
 {
-    char    type;
-    char    *strNum;
-    int     width;
-    int     precision;
-} t_flags;
+	char	type;
+	char	*strNum;
+	int		width;
+	int		precision;
+}	t_flags;
 
-int     ft_printf(const char *format, ...);
+int		ft_printf(const char *format, ...);
 void	ft_putchar_len(char c, int *len);
 int		ft_strchr_01(char *s, char c);
-
-//para imprimir integer:
 size_t	ft_strlen(const char *s);
 void	ft_putstr(char *s);
 void	ft_putchar(char c);
@@ -43,24 +39,18 @@ void	ft_putnbr(int n);
 char	*ft_itoa(int n);
 void	ft_putstr_len(char *s, int *len);
 void	print_s(char *c, int *len);
-void    print_i_d(t_flags fl, va_list args, int *len);
-
-void    print_u(t_flags fl, va_list args, int *len);
+void	print_i_d(t_flags fl, va_list args, int *len);
+void	print_u(t_flags fl, va_list args, int *len);
 char	*ft_uitoa(unsigned int n);
-int 	ft_ulen(unsigned int num);
+int		ft_ulen(unsigned int num);
 void	print_c(char c, int *len);
-
-
 void	print_p(t_flags fl, va_list args, int *len);
-char    *ft_int_to_hex_px(unsigned long int n);
+char	*ft_int_to_hex_px(unsigned long int n);
 void	ft_putstr_len_p(char *s, int *len);
 int		ft_len_hex(unsigned long int x);
-
 void	print_x(t_flags fl, va_list args, int *len);
-
 char	*ft_int_to_hex_X(unsigned long int n);
 void	print_X(t_flags fl, va_list args, int *len);
-
-void print_pct(int *len);
+void	print_pct(int *len);
 
 #endif
